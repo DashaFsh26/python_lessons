4597cee6491bdbfa4c399ea2356a8394000f430b
Daria Chirkova <darya.churova@inbox.ru>
2024-09-09 18:59:54 +0300

    3

---
+++ lesson_6/1.py
@@ -0,0 +1,50 @@
+# Сравнения
+'''
+<
+<=
+>
+>=
+==
+is
+'''
+
+# Numbers
+# int
+print(1 > 4)
+print(1 >= 1)
+print(1 == 1)
+
+a = 1 + 5 >= 4 - 2
+print(a)
+
+a = 10000267
+b = 10000267
+
+print(a == b)
+print(a is b)
+# float
+
+
+# String
+print("Strings: ")
+print("oleg" == "dasha")
+print("oleg" == "oleg")
+print("oleg" is "oleg")
+
+print("a" >= "b")
+print("ba" >= "bc")
+
+# bool
+print("Bool: ")
+a = "some"
+print(a is False)
+
+
+# None
+print("None: ")
+a = "some"
+print(a is None)
+
+
+# Составные типы данных
+# list
